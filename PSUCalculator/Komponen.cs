8e232f34f97db1b5d90ffe61aaff9f2a0c971714
karuniaperjuangan <71960985+karuniaperjuangan@users.noreply.github.com>
2021-04-09 17:19:01 +0700

    Add files via upload
    Initial Upload

---
+++ PSUCalculator/Komponen.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSUCalculator
+{
+
+    public abstract class PCComponent
+    {
+        public string name;
+        public double powerDrawn;
+
+        public PCComponent(string Name)
+        {
+            this.name = Name;
+        }
+
+        public virtual double DrawPower()
+        {
+            return powerDrawn;
+        }
+    }
+
+}
---
+++ PSUCalculator/RAM.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSUCalculator
+{
+    public class RAM : PCComponent
+    {
+        public RAM(string Name) : base(Name) { }
+        public int memory_size;
+
+        public override double DrawPower()
+        {
+            return 1/2 * memory_size;
+        }
+    }
+    // Ternyata pada teknologi saat ini, RAM lebih hemat 75%
+    public class RevisedRAM : RAM
+    {
+        public RevisedRAM(string Name) : base(Name) { }
+
+        public override double DrawPower()
+        {
+            return base.DrawPower() * 0.75;
+        }
+    }
+}
